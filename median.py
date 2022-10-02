193f639dd2cf5031917f3c62e2872c11d1f4a7b0
Aman Noor <aman.noor@kcl.ac.uk>
2022-10-02 16:11:03 +0100

    Complete task

--- median.py
+++ median.py
@@ -1,6 +1,7 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -10,3 +11,14 @@ while True:
     else:
         break
 print(numbers)
+
+numbers.sort()
+length = len(numbers)
+if length % 2 == 1:
+    position = int((length - 1) / 2)
+    median = numbers[position]
+else:
+    position1 = int(length / 2)
+    position2 = int((length / 2) - 1)
+    median = (numbers[position1] + numbers[position2]) / 2
+print(f'{median} is the median of this list')
